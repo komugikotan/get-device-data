25bca6c51269e13f27cc9e1cbcfa2cfcd3cbc81b
こむぎこたん <90941894+komugikotan@users.noreply.github.com>
2021-09-28 09:01:34 +0900

    Add files via upload

--- js/get.js
+++ js/get.js
@@ -2,65 +2,39 @@ var userAgent = window.navigator.userAgent.toLowerCase();
 
 if(userAgent.indexOf('msie') != -1 ||
         userAgent.indexOf('trident') != -1) {
-    console.log('Internet Explorer');
-    document.getElementById('browser-name').innerHTML = '<p>　Internet Explorer</p>';
+            document.getElementById('browser-name').innerHTML = "Internet Explorer";
 } else if(userAgent.indexOf('edge') != -1) {
-    console.log('Edge');
-    document.getElementById('browser-name').innerHTML = '<p>　Edge</p>';
+    document.getElementById('browser-name').innerHTML = "Microsoft Edge";
 } else if(userAgent.indexOf('chrome') != -1) {
-    console.log('Google Chrome');
-
-    document.getElementById('browser-name').innerHTML = '<p>　Google Chrome</p>';
+    document.getElementById('browser-name').innerHTML = "Google Chrome";
 } else if(userAgent.indexOf('safari') != -1) {
-    console.log('Safari');
-    document.getElementById('browser-name').innerHTML = '<p>　Safari</p>';
+    document.getElementById('browser-name').innerHTML = "Safari";
 } else if(userAgent.indexOf('firefox') != -1) {
-    document.getElementById('browser-name').innerHTML = '<p>　firefox</p>';
-    console.log('FireFox');
+    document.getElementById('browser-name').innerHTML = "Fire Fox";
 } else if(userAgent.indexOf('opera') != -1) {
-    console.log('Opera');
-    document.getElementById('browser-name').innerHTML = '<p>　Opera</p>';
+    document.getElementById('browser-name').innerHTML = "Opera";
 } else {
-    console.log('お使いのブラウザを調べることができませんでした');
-    document.getElementById('browser-name').innerHTML = '　お使いのブラウザを調べることができませんでした';
+    document.getElementById('browser-name').innerHTML = "ブラウザ名がリストにありません。代わりに、ブラウザ情報をそのまま表示します。" + "「" + userAgent + "」";
 }
 
-var userOS = window.navigator.userAgent.toLowerCase();
-
-if(userOS.indexOf("windows nt") !== -1) {
-  console.log("Microsoft Windows");
-  document.getElementById('os-name').innerHTML = '<p>　Microsoft Windows</p>';
-} else if(userOS.indexOf('android') !== -1) {
-  console.log("Android");
-  document.getElementById('os-name').innerHTML = '<p>　Android</p>';
-} else if(userOS.indexOf('iphone') !== -1 || ua.indexOf("ipad") !== -1) {
-  console.log("iOS");
-  document.getElementById('os-name').innerHTML = '<p>　iOS</p>';
-} else if(userOS.indexOf(('mac os x') !== -1)) {
-  console.log("macOS");
-  document.getElementById('os-name').innerHTML = '<p>　macOS</p>';
-} else {
-  document.getElementById('os-name').innerHTML = '<p>　お使いのOSを調べることができませんでした。</p>';
-  console.log("お使いのOSを調べることができませんでした");
-}
-
-var language = (window.navigator.languages && window.navigator.languages[0]) ||
-            window.navigator.language ||
-            window.navigator.userLanguage ||
-            window.navigator.browserLanguage;
 
 
-document.getElementById('language-name').innerHTML = '　' + language;
+if(userAgent.indexOf("windows nt") !== -1) {
+    document.getElementById('os-name').innerHTML = "Microsoft Windows";
+  } else if(userAgent.indexOf("android") !== -1) {
+    document.getElementById('os-name').innerHTML = "Android";
+  } else if(userAgent.indexOf("iphone") !== -1 || userAgent.indexOf("ipad") !== -1) {
+    document.getElementById('os-name').innerHTML = "iOS, or iPad OS";
+  } else if(userAgent.indexOf("mac os x") !== -1) {
+    document.getElementById('os-name').innerHTML = "macOS";
+  } else {
+    console.log("OS名がリストにありません。");
+  }
 
-$.ajax({
-    url: "https://ipinfo.io",
-    dataType: "jsonp",
-    success: function(res){
-        document.getElementById('ip-address').innerHTML = '　' + (res.ip);
-    }
-});
 
-var sw = window.screen.width; //横幅
-var sh = window.screen.height; //高さ
+var lang = navigator.language;
+document.getElementById('language-name').innerHTML = lang;
 
-document.getElementById('display-size').innerHTML = '　' +document.body.clientWidth + '×' + document.body.clientHeight;
+fetch('https://ipinfo.io?callback')
+  .then(res => res.json())
+  .then(json => document.getElementById('ip-address').innerHTML = json.ip);
